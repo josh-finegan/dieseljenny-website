18ea6f28e69d509593eef93dd1a590ae2f7a3ede
Josh Finegan <josh@Mac.lan>
2025-08-10 18:54:50 -0600

    feat: build initial splash page

---
+++ lib/config.ts
@@ -0,0 +1,15 @@
+// lib/config.ts
+export const navLinks = [
+  {
+    href: "https://bandcamp.com/your-profile",
+    label: "Music",
+  },
+  {
+    href: "https://youtube.com/your-channel",
+    label: "Videos",
+  },
+  {
+    href: "https://instagram.com/your-profile",
+    label: "Socials",
+  },
+];
